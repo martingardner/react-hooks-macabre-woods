c03c6e2b19533f12180063d6a094292da2fa17ec
martin gardner II <martingardnerii@gmail.com>
2019-05-31 11:19:05 -0500

    Updated components tree added in headSection and header

---
+++ macabre-forest/src/components/Main.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from './headSection/Header';
+
+const Main = () => {
+
+    return(
+        <div>
+            <Header />
+
+        </div>
+    )
+}
+
+export default Main
---
+++ macabre-forest/src/components/headSection/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+
+    return (
+        <div>
+            Max Number of Workers : 4
+        </div>
+    )
+}
+
+export default Header
