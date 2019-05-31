686e3ef044d6f562bd6e0ba177becaec588e2ab8
martin gardner II <martingardnerii@gmail.com>
2019-05-31 13:51:06 -0500

    base WorkerForm added and attached to main component

--- macabre-forest/src/components/Main.js
+++ macabre-forest/src/components/Main.js
@@ -2,15 +2,17 @@ import React, {useReducer} from 'react';
 import {InitValues, DataReducer} from '../reducer/DataReducer';
 import DataContext from '../context/DataContext';
 import Header from './headSection/Header';
+import WorkerForm from './workerSection/WorkerForm';
 
 
 const Main = () => {
-    const [dataReducer, dispatch] = useReducer( DataReducer, InitValues );
+    const [dataReducer, dispatch] = useReducer(DataReducer, InitValues);
 
     return(
         <DataContext.Provider value={{dataReducer, dispatch}}>
             <div>
                 <Header />
+                <WorkerForm />
             </div>
         </DataContext.Provider>
     )
---
+++ macabre-forest/src/components/workerSection/WorkerForm.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const WorkerForm = () => {
+
+    return (
+        <div>
+            <div>
+                <div>Worker</div>
+                <div>Location</div>
+            </div>
+            <form>
+
+            </form>
+        </div>
+    )
+}
+
+export default WorkerForm
